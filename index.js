32cb5920bdc9aa267294a8c00896a592c27135f6
shubham-shinde <shindes0321@gmail.com>
2018-03-22 18:31:27 +0530

    html rendering added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/',(req, res) => {
-    res.send('express working');
+    res.sendFile(path.join(__dirname,'public','konfinity.html'));
 })
 
 app.listen(port, () => {
